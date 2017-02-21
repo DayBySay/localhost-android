f5b1d525a6c61c16ede4035a37fb0494779e9db4
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-02-21 21:02:43 +0900

    リスナーの追加

--- app/src/main/java/jp/fluct/localhost_android/MainActivity.kt
+++ app/src/main/java/jp/fluct/localhost_android/MainActivity.kt
@@ -1,8 +1,10 @@
 package jp.fluct.localhost_android
 
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
@@ -13,11 +15,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val webview = findViewById(R.id.webview) as WebView
-        webview.loadUrl("file:///android_asset/index.html")
         webview.setWebViewClient(object: WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+            }
+            override fun shouldInterceptRequest(view: WebView?, request: WebResourceRequest?): WebResourceResponse {
+                return super.shouldInterceptRequest(view, request)
+            }
+
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
                 return super.shouldOverrideUrlLoading(view, request)
             }
         })
+        webview.loadUrl("file:///android_asset/index.html")
     }
 }
